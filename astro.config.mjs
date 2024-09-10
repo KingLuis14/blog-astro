cde3495e6ab05e7c40856d3b77a5417c1b86962f
Luis Enrique <enriquecalero284@gmail.com>
2024-09-10 14:56:09 -0500

    Astro solo post redireccina prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
-  // redirects: {
-  //   '/posts/[tag]': '/',
-  //   '/posts': '/',
-  // },
+  redirects: {
+    '/posts': '/',
+  },
 });
